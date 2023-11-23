df9ddfaa078b8f60f1d5834d80debb8bb727b325
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-11-23 18:58:13 +0530

    new test

---
+++ my-project/src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { medicineData } from './varible';
+import dayjs from 'dayjs';
+import _ from 'lodash';
+import { FaSun, FaMoon, FaCoffee } from 'react-icons/fa';
+
+
+
+
+function App() {
+  const sameDateData = _.groupBy(medicineData, 'created_at');
+  const items = Object.entries(sameDateData);
+  const [details, setDetails] = useState()
+  let data = "";
+
+  if (details) {
+    const sameTime = _.groupBy(details, 'slot_time');
+    data = Object.entries(sameTime);
+  }
+  
+  // console.log(data)
+  return (
+    <>
+      <div className='flex justify-between flex-wrap'>
+        {items.map(([date, itemsArray]) => {
+          const formattedDate = dayjs(date).format('DD');
+          const dayOfWeek = dayjs(date).locale('en').format('ddd');
+
+          return (
+            <div key={formattedDate} className={`m-2 border-2 h-24 w-20 max-w-xs overflow-wrap rounded-md cursor-pointer p-2 `}
+              onClick={() => setDetails(itemsArray)}>
+              <p>{formattedDate}</p>
+              <p>{dayOfWeek}</p>
+
+            </div>
+          );
+        })}
+      </div>
+
+      {data &&
+        <div>
+          <div>
+            {data.map(([slotTime, slotItems]) => (
+              <div key={slotTime} className='flex justify-between border-2 border-blue-500 m-2 p-2'>
+                <div>
+                  {
+                    slotItems.map((item) =>
+                      <div>
+                        <p key={item.id}>{item.medicine_name}</p>
+                      </div>
+                    )
+                  }
+                </div>
+                <p className='text-2xl font-bold'>{slotTime}</p>
+              </div>
+            ))}
+          </div>
+        </div>}
+    </>
+  );
+}
+
+export default App;
+
+
+
